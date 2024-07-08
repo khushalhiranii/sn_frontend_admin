c5705407dfdf6af7c8c28f2a23a2984c42470450
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-08 16:08:31 +0530

    compiled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+// import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "./context/AuthContext";
 
