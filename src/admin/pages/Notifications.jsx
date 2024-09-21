db2335573f4634b71369cbc727875135fff76bcf
khushalhiranii <khushal.hirani4@gmail.com>
2024-09-21 14:57:20 +0530

    index and package updated

--- src/admin/pages/Notifications.jsx
+++ src/admin/pages/Notifications.jsx
@@ -4,31 +4,33 @@ import { useAdminSocket } from '../context/AdminSocketContext'
 function Notifications() {
   const { notifications } = useAdminSocket();
   console.log(notifications)
+  console.log(typeof(notifications))
   const notificationArray = Object.values(notifications)
   console.log(notificationArray)
-  const filteredNotifications = notificationArray.filter((notification) => notification.categoty === "Admin")
-  console.log(filteredNotifications)
+  
+  // const filteredNotifications = notificationArray.filter((notification) => notification.Categoty === "Admin")
+  // console.log(filteredNotifications)
   return (
     <div className='flex-1 flex flex-col py-4  max-w-[calc(100%_-_344px)] gap-[16px]'>
       <div className='text-[32px] font-semibold'>Notification</div>
-      {filteredNotifications.map((notification, index) => (
+      {notificationArray.map((notification, index) => (
         <div key={index} className='flex flex-col gap-[3px] p-4 rounded-[4px] bg-[#E6F0FC]'>
         <div className='flex flex-row justify-between'>
         <div className='text-[20px] font-medium'>{notification.Title}</div>
-        <div>DD/Time</div>
+        <div>{typeof(notification.Created)}</div>
         </div>
         <div>
-        Customer ID : ************
+        Description : {notification.Description}
         </div>
         <div>
-        Loan Type : ************
+        Customer ID : {notification.Identifier}
         </div>
         <div>
-        Due in Day's : ************
+        Loan ID : {notification.Reference}
         </div>
-        <div>
+        {/* <div>
         Due Amount : ************
-        </div>
+        </div> */}
       </div>
       ))}
       {/* <div className='flex flex-col w-full gap-[8px]'>
