c227e2ef35aefb536e9dbb2a78b81a282e0e1411
khushalhiranii <khushal.hirani4@gmail.com>
2024-11-04 19:30:15 +0530

    open account

--- src/user/DesignSystem/Dropdown.jsx
+++ src/user/DesignSystem/Dropdown.jsx
@@ -19,11 +19,11 @@ const DropdownMenu = () => {
       </div>
       {isOpen && (
         <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
-          <Link to="/" className="block px-4 py-2 text-black hover:bg-gray-200">Home</Link>
-          <Link to="/schemes" className="block px-4 py-2 text-black hover:bg-gray-200">Schemes</Link>
-          <Link to="/loans" className="block px-4 py-2 text-black hover:bg-gray-200">Loans</Link>
-          <Link to="/saving-account" className="block px-4 py-2 text-black hover:bg-gray-200">Saving Account</Link>
-          <Link to="/contact" className="block px-4 py-2 text-black hover:bg-gray-200">Contact Us</Link>
+          <Link to="/" className="block px-4 py-2 text-black no-underline hover:bg-gray-50">Home</Link>
+          <Link to="/schemes" className="block px-4 py-2 text-black no-underline hover:bg-gray-50">Schemes</Link>
+          <Link to="/loans" className="block px-4 py-2 text-black no-underline hover:bg-gray-50">Loans</Link>
+          <Link to="/saving-account" className="block px-4 py-2 text-black no-underline hover:bg-gray-50">Saving Account</Link>
+          <Link to="/contact" className="block px-4 py-2 text-black no-underline hover:bg-gray-50">Contact Us</Link>
           {/* <Link to="/search" className="block px-4 py-2 text-black hover:bg-gray-200">Search</Link>
           <Link to="/notifications" className="block px-4 py-2 text-black hover:bg-gray-200">Notifications</Link>
           <Link to="/user" className="block px-4 py-2 text-black hover:bg-gray-200">User</Link> */}
